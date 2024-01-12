4c1b9c3c5cfabd1c0f86494f957fb3c298696410
Katriina Räisänen <kultakatriinasdf@gmail.com>
2024-01-12 23:01:44 +0200

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItem = document.querySelectorAll('.nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 // Set initial state of menu
 let showMenu = false;
@@ -11,12 +11,12 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
+    if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItem.forEach(item => item.classList.add('show'));
+        navItems.forEach(item => item.classList.add('show'));
 
         // set menu state
         showMenu = true;
@@ -25,7 +25,7 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItem.forEach(item => item.classList.remove('show'));
+        navItems.forEach(item => item.classList.remove('show'));
 
         // set menu state
         showMenu = false;
